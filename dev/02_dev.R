c851e102292bb3511a50d8b5106f457f04f6a648
zander-prinsloo <prinsloo.zander@gmail.com>
2025-07-02 19:51:42 -0400

    add data

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -37,7 +37,9 @@ golem::add_any_file("file.json")
 
 ## Add internal datasets ----
 ## If you have data in your package
-usethis::use_data_raw(name = "my_dataset", open = FALSE)
+usethis::use_data_raw(name = "dcol", open = FALSE)
+usethis::use_data_raw(name = "d_dm", open = FALSE)
+usethis::use_data_raw(name = "d_stb", open = FALSE)
 
 ## Tests ----
 ## Add one line by test you want to create
