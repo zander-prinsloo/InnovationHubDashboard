6c7b7c5bc4063b55ef63f858f8bb24bce08c4d04
zander-prinsloo <prinsloo.zander@gmail.com>
2026-01-20 13:33:10 -0500

    add logos/designs and header/navbar

--- R/app_ui.R
+++ R/app_ui.R
@@ -7,8 +7,58 @@
 app_ui <- function(request) {
   tagList(
     golem_add_external_resources(),
+    
+    # ---- PIP Header ----
+    tags$header(
+      class = "pip-header",
+      
+      # Logo on the left
+      tags$div(
+        class = "pip-header-logo",
+        tags$img(
+          src = "www/pip-logo.png",
+          alt = "World Bank Poverty and Inequality Platform Logo"
+        )
+      ),
+      
+      # Navigation menu on the right
+      tags$nav(
+        tags$ul(
+          class = "pip-header-nav",
+          
+          tags$li(
+            class = "pip-header-nav-item",
+            tags$a(
+              href = "https://pip.worldbank.org/home",
+              class = "pip-header-nav-link",
+              target = "_blank",
+              "PIP Home"
+            )
+          ),
+          
+          tags$li(
+            class = "pip-header-nav-item",
+            tags$span(
+              class = "pip-header-nav-link active",
+              "Deep Dives"
+            )
+          ),
+          
+          tags$li(
+            class = "pip-header-nav-item",
+            tags$a(
+              href = "https://avsolatorio.github.io/ai-for-data-blog/semantic-search/ids-doc.html",
+              class = "pip-header-nav-link",
+              target = "_blank",
+              "Research Repository"
+            )
+          )
+        )
+      )
+    ),
+    
+    # ---- Main dashboard content ----
     fluidPage(
-      # ---- your dashboard module ----
       mod_interactive_dashboard_ui("interactive_dashboard_1")
     )
   )
@@ -34,6 +84,12 @@ golem_add_external_resources <- function() {
     bundle_resources(
       path = app_sys("app/www"),
       app_title = "InnovationHubDashboard"
+    ),
+    # Load header CSS
+    tags$link(
+      rel = "stylesheet",
+      type = "text/css",
+      href = "www/header.css"
     )
     # Add here other external resources
     # for example, you can add shinyalert::useShinyalert()
